0434b46af0048be085619ff4918c90fb30505108
Nazarii <nazarrishchuk@gmail.com>
2021-03-18 16:01:48 +0200

    Updates

---
+++ precache-manifest.a81a1b60a811334bb328faebcbc96da6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b16a6220191fc0797c16e6d3445125d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4bb9558e2bc548725bf8",
+    "url": "./static/css/main.39b6a495.chunk.css"
+  },
+  {
+    "revision": "23b539dc889cac83f0a1",
+    "url": "./static/js/2.cb711649.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cb711649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb9558e2bc548725bf8",
+    "url": "./static/js/main.bbb233d0.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  },
+  {
+    "revision": "c0c003384035ba3df9647cb2d2d965b8",
+    "url": "./static/media/Spinner-3.c0c00338.gif"
+  }
+]);
